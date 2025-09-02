87c9a449a3b36f39880e2fe35a19787256feb92e
Shree <shree@Shrees-iMac.local>
2025-09-02 15:33:41 +0530

    Update header to use logo.svg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
             <img 
               src="/logo.svg" 
               alt="MM Pro Biz Services" 
-              className="h-10 w-auto" 
+              className="h-14 w-auto" 
             />
           </div>
 
